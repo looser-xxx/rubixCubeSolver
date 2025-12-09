f8239978c0eebd60a6f83d204b7ff97e25940f1b
Suprabhat Maurya <itismaurya4769@gmail.com>
2025-12-09 16:27:24 +0530

    feat: add isValidState helper function
    Previously, isValidState threw a runtime error if the state object
    was null. This change adds a guard clause to return false immediately
    if the input is null, preventing the application crash.

--- main.py
+++ main.py
@@ -10,40 +10,64 @@ class Cube:
     def __init__(self, stateData) -> None:
         self.state=np.array(stateData)
 
+
+
+
+
     def isValidState(self):
-        redStickerCount=0
-        yellowStickerCount=0
-        whiteStickerCount=0
-        blueStickerCount=0
-        greenStickerCount=0
-        orangeStickerCount=0
 
-        for i in range(6):
-            for values in self.state[i]:
-                match values:
-                    case 'r':
-                        redStickerCount+=1
-                    case 'y':
-                        yellowStickerCount+=1
-                    case 'w':
-                        whiteStickerCount+=1
-                    case 'b':
-                        blueStickerCount+=1
-                    case 'g':
-                        greenStickerCount+=1
-                    case 'o':
-                        orangeStickerCount+=1
-                    case _:
-                        print(f"unknown move: {values}")
+        allStickers=np.array(self.state).flatten()
 
-        if redStickerCount == yellowStickerCount == whiteStickerCount == blueStickerCount == greenStickerCount == orangeStickerCount==9):
+        unique, counts = np.unique(allStickers, return_counts=True)
+        
+        countDict=dict(zip(unique, counts))
+
+        if len(unique)==6 and all(c==9 for c in counts):
             return True
+        
+        print(f"Invalid State: {countDict}")
+        return False
+
+
+
+<<<<<<< HEAD
+    def moveRed(self, clockWise=True):
+        # Temp stores Yellow Bottom Row (Row 2)
+        temp = np.array([self.state[0][2][0], self.state[0][2][1], self.state[0][2][2]])
+        
+        if clockWise:
+            self.state[1] = np.rot90(self.state[1], k=-1)
+            
+            # 1. Yellow Bottom Row (Row 2) gets Blue Right Col (Col 2) [Reversed]
+            self.state[0][2][0], self.state[0][2][1], self.state[0][2][2] = self.state[4][2][2], self.state[4][1][2], self.state[4][0][2]
+            
+            # 2. Blue Right Col (Col 2) gets White Top Row (Row 0) [Reversed]
+            self.state[4][2][2], self.state[4][1][2], self.state[4][0][2] = self.state[2][0][2], self.state[2][0][1], self.state[2][0][0]
+            
+            # 3. White Top Row (Row 0) gets Green Left Col (Col 0) [Reversed]
+            self.state[2][0][2], self.state[2][0][1], self.state[2][0][0] = self.state[5][0][0], self.state[5][1][0], self.state[5][2][0]
+            
+            # 4. Green Left Col (Col 0) gets Temp (Yellow) [Reversed]
+            # Note: Your original code reversed the temp assignment (temp[2], temp[1]...)
+            self.state[5][0][0], self.state[5][1][0], self.state[5][2][0] = temp[2], temp[1], temp[0]
+            
         else:
-            return False
-
-
-
-
+            self.state[1] = np.rot90(self.state[1], k=1)
+            
+            # 1. Yellow Bottom Row gets Green Left Col
+            self.state[0][2][0], self.state[0][2][1], self.state[0][2][2] = self.state[5][0][0], self.state[5][1][0], self.state[5][2][0]
+            
+            # 2. Green Left Col gets White Top Row
+            self.state[5][0][0], self.state[5][1][0], self.state[5][2][0] = self.state[2][0][2], self.state[2][0][1], self.state[2][0][0]
+            
+            # 3. White Top Row gets Blue Right Col
+            self.state[2][0][2], self.state[2][0][1], self.state[2][0][0] = self.state[4][2][2], self.state[4][1][2], self.state[4][0][2]
+            
+            # 4. Blue Right Col gets Temp (Yellow)
+            self.state[4][2][2], self.state[4][1][2], self.state[4][0][2] = temp[0], temp[1], temp[2]
+=======
+    
+>>>>>>> f844a81 (feat: add isValidState helper function)
 
 
 
